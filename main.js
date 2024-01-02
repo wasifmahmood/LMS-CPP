1bf6dc36ed7fba65589924176be1f0a3c0a534a4
Wasif Mahmood <116950247+wasifmahmood@users.noreply.github.com>
2024-01-02 22:07:08 +0500

    switch case complete

--- main.js
+++ main.js
@@ -5,91 +5,30 @@ import Library from "./src/Library.js";
 import Student from "./src/Student.js";
 import readline from 'readline';
 
+
 const rl = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout
- });
- 
-function main() {
+  input: process.stdin,
+  output: process.stdout
+});
 
-    // Create author
-    let author  = new Author("sumair");
-    let auther2 = new Author("Wasif")
-    let auther3 = new Author("Zahid")
-
-
-   //  // Create books
-    let book1 = new Book("Harry Potter and the Sorcerer's Stone", "978", "Fantasy");
-    let book2 = new Book("The Lord of the Rings", "979", "Romantic");
-    let book3 = new Book("The Hobbit", "07122", "Computer");
-
-   //  // setAuthor is a Book
-    book1.setAuthor(author);
-    book2.setAuthor(auther3)
-    book3.setAuthor(auther2);
-
-   //  // Function calling
-    book1.borrowBook();
-    book1.returnBook();
-    book1.calculateFine();
-
-    console.log(book1);
-    
-
-//     // Create Student
-   //  let studentName = new Student("Hamza","219596","Hamza@gmail.com");
-
-   //  studentName.borrowBook();
-   //  studentName.returnBook();
-
-   //  console.log(studentName);
-
-//  // Create Librarian
-    let librarianName   = new Librarian("Hameed");
-
-   //  librarianName.addBook();
-   //  librarianName.removeBook();
-   //  librarianName.shelveBook();
-   //  librarianName.issueBook();
-   //  librarianName.receiveBook();
-
-   //  console.log(librarianName);
-
-
- // Create Library
-    let address   = new Library("DHA Phase 1");
-
-    address.registerStudent();
-    address.removeStudent();
-    address.registerLibrarian();
-    address.removeLibrarian();
-
-
-    console.log(address);
-
-    //input
-
-
-   
-
-
-
-while (true) {
+function getUserInput(address) {
   console.log("1. Register Librarian\n2. Remove Librarian\n3. Register Student\n4. Remove Student\n5. Add Book\n6. Remove Book\n7. Borrow Book\n8. Return Book\n9. Exit\n");
 
   rl.question("Enter your choice:", (choice) => {
     choice = parseInt(choice);
 
+
     switch (choice) {
       case 1: {
-        let librarianName = prompt("Enter Librarian Name:");
-        let newLibrarian = new Librarian(librarianName);
-        Library.registerLibrarian(newLibrarian);
+          address.registerLibrarian();
+          console.log(address);
         break;
       }
       case 2: {
-        let librarianName = prompt("Enter Librarian Name to Remove:");
-        Library.removeLibrarian(librarianName);
+        rl.question('Enter Remove librarian Name: ', (input) => {
+        let librarianName = input;
+        address.removeLibrarian(librarianName);
+      });
         break;
       }
       case 3: {
@@ -115,8 +54,8 @@ while (true) {
         break;
       }
       case 6: {
-        let bookTitle = prompt("Enter Book Title to Remove:");
-        Librarian.removeBook(bookTitle);
+        let bookISBN = prompt("Enter Book ISBN to Remove:");
+        Librarian.removeBook(bookISBN);
         break;
       }
       case 7: {
@@ -143,17 +82,74 @@ while (true) {
         return;
       default:
         console.log("Invalid choice. Try again.");
-    }
+      }
+      process.nextTick(getUserInput);
+
   });
+
 }
 
 
 
 
+function main() {
+
+  // Create author
+  let author = new Author("sumair");
+  let auther2 = new Author("Wasif")
+  let auther3 = new Author("Zahid")
 
 
+  //  // Create books
+  let book1 = new Book("Harry Potter and the Sorcerer's Stone", "978", "Fantasy");
+  let book2 = new Book("The Lord of the Rings", "979", "Romantic");
+  let book3 = new Book("The Hobbit", "07122", "Computer");
 
-    return 0;
+  //  // setAuthor is a Book
+  book1.setAuthor(author);
+  book2.setAuthor(auther3)
+  book3.setAuthor(auther2);
+
+  //  // Function calling
+  book1.borrowBook();
+  book1.returnBook();
+  book1.calculateFine();
+
+  console.log(book1);
+
+
+  //     // Create Student
+  //  let studentName = new Student("Hamza","219596","Hamza@gmail.com");
+
+  //  studentName.borrowBook();
+  //  studentName.returnBook();
+
+  //  console.log(studentName);
+
+  //  // Create Librarian
+  let librarianName = new Librarian("Hameed");
+
+  //  librarianName.addBook();
+  //  librarianName.removeBook();
+  //  librarianName.shelveBook();
+  //  librarianName.issueBook();
+  //  librarianName.receiveBook();
+
+  //  console.log(librarianName);
+
+
+  // Create Library
+  var address = new Library("DHA Phase 1");
+
+  address.registerStudent();
+  address.removeStudent();
+  address.registerLibrarian();
+  address.removeLibrarian();
+
+
+  console.log(address);
+  getUserInput(address)
+  return 0;
 }
 
 main();
